fea1cc3f356cc17dc87ef4c5d7b2d0b424ac625d
GalacticGlum <nerftactic09@gmail.com>
2020-04-01 09:56:11 -0400

    Added tensorflow-gpu requirement into environment.yml

---
+++ composer/models/music_rnn.py
@@ -0,0 +1,7 @@
+'''
+MusicRNN: A recurrent neural network model designed to generate music based on an
+MIDI-like event-based description language (see: ``composer.dataset.sequence`` for
+more information about the event-based sequence description.)
+
+'''
+

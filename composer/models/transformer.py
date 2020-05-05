4b9082d352bfaf60650b344a462c6a5ae7ed5bfa
GalacticGlum <nerftactic09@gmail.com>
2020-05-05 11:51:58 -0400

    Staging

--- composer/models/transformer.py
+++ composer/models/transformer.py
@@ -55,6 +55,7 @@ def attention_mask(size):
          [0., 0., 1., 1.],
          [0., 0., 0., 1.],
          [0., 0., 0., 0.]]
+         
     '''
 
     mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
